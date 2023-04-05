c590b8972eab4a23ee0d03503ec99f59bc43e9e6
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-04-05 21:12:48 +1000

    Add Emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,22 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import React from "react";
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh" },
+    4: { src: thumbsUp, alt: "recommended" },
+    5: { src: bullsEye, alt: "exceptional" },
+  };
+  return <Image marginTop={1} {...emojiMap[rating]} boxSize='25px'></Image>;
+};
+
+export default Emoji;
