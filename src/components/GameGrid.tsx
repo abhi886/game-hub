105549d9aa1be409ecd0918933c5901ff51d1708
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-05-11 12:17:29 +1000

    Fix the skeleton width while loading games in index page

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,31 @@
-import { list, Text, SimpleGrid } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
-import { Platform } from "../hooks/usePlatforms";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import { GameQuery } from "../App";
+import useGames, { Platform } from "../hooks/useGames";
+import { Genre } from "../hooks/useGenres";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
   gameQuery: GameQuery;
 }
+
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
   if (error) return <Text>{error}</Text>;
+
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding='10px'
       spacing={6}
-      padding='10'
     >
       {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
-            {" "}
-            <GameCardSkeleton></GameCardSkeleton>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
       {data.map((game) => (
